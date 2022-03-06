ade9163e2fcf3693bff72e2842e3a0c96d609d1d
Edvaldo I Souza <edvaldoivosouza@ymail.com>
2022-03-06 10:52:46 -0300

    Correção da conexao com DB

--- database/migrations/2022_03_06_010807_create__jogos_table.php
+++ database/migrations/2022_03_06_010807_create__jogos_table.php
@@ -13,14 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+        Schema::create('_jogos', function (Blueprint $table) {
+            $table->id('identidade',2);
+            $table->string('NomeJogo',15);
+            $table->double('ValorJogo',8,2);
+            $table->string('Classificação',2);
+            $table->timestamps();
+           
         });
     }
 
@@ -31,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('_jogos');
     }
 };
